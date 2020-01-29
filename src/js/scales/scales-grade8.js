509fb4d660b9ee8f97830d3fcce347f4fefdfa37
Stephen Leigh <s-leigh@users.noreply.github.com>
2020-01-29 17:20:15 +0000

    Tidy scale generation

--- src/js/scales/scales-grade8.js
+++ src/js/scales/scales-grade8.js
@@ -1,42 +1,113 @@
-const allScales = [];
+const DIATONIC_OCTAVES = 4
+const DIATONIC_OCTAVES_SCALE_TEMPO = 88
+const DIATONIC_SIXTH_THIRDS_SCALE_TEMPO = 63
+const LEGATO_SCALES_THIRDS_TEMPO = 52
+const LEGATO_SCALES_OCTAVES = 2
+const CHROMATIC_THIRD_APART_OCTAVES = 4
+const CHROMATIC_THIRD_APART_TEMPO = 76
+const CHROMATIC_MINOR_THIRDS_OCTAVES = 2
+const CHROMATIC_MINOR_THIRDS_TEMPO = 52
+const WHOLE_TONE_SCALE_OCTAVES = 2
+const WHOLE_TONE_SCALE_TEMPO = 88
+const ARPEGGIO_OCTAVES = 4
+const ARPEGGIO_TEMPO = 66
+const STYLES = {LEGATO: 'legato', STACCATO: 'staccato'}
+const ALL_NOTES = ['C', 'C♯/D♭', 'D', 'E♭', 'E', 'F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'B♭', 'B']
+
+class FingerExercise {
+    constructor(interval, hands, mode, octaves, startingNote, style, tempo, type) {
+        this.interval = interval
+        this.hands = hands
+        this.mode = mode
+        this.octaves = octaves
+        this.startingNote = startingNote
+        this.style = style
+        this.tempo = tempo
+        this.type = type
+    }
+}
+
+class DiatonicScaleOctaves extends FingerExercise {
+    constructor(interval, startingNote, style, mode, tempo) {
+        super(interval, 'hands together', mode, DIATONIC_OCTAVES, startingNote, style, tempo, 'Scale in octaves')
+    }
+}
+
+class DiatonicScaleThirds extends FingerExercise {
+    constructor(startingNote) {
+        super('third apart', 'hands separately', 'major', LEGATO_SCALES_OCTAVES, startingNote, STYLES.LEGATO, LEGATO_SCALES_THIRDS_TEMPO, 'scale a third apart')
+        this.startingNote = startingNote
+    }
+}
+
+class Arpeggio extends FingerExercise {
+    constructor(inversion, mode, startingNote, arpeggioType) {
+        super('octave apart', 'both hands', mode, ARPEGGIO_OCTAVES, startingNote, STYLES.LEGATO, ARPEGGIO_TEMPO)
+        this.inversion = inversion
+        this.arpeggioType = arpeggioType
+        this.type = 'arpeggio'
+    }
+}
+
+class DominantSeventh extends Arpeggio {
+    constructor(key) {
+        super('root', 'major', null, 'dominant seventh')
+        this.key = key
+        this.type = 'dominant seventh'
+    }
+}
 
 const standardScaleStartingNotes = ['C', 'D', 'B', 'F♯', 'F', 'E♭', 'A♭/G♯', 'D♭/C♯'];
-const modes = ['major', 'minor harmonic', 'minor melodic'];
-const keys = standardScaleStartingNotes.map(note => modes.map(mode => `${note} ${mode}`)).flat();
-const intervals = ['octave apart', 'third apart', 'sixth apart'];
-const bothStyles = ['legato', 'staccato'];
-keys.forEach(key => allScales.push({key, intervals: key.includes('melodic') ? ['octave apart'] : intervals, style: bothStyles}));
+const diatonicScaleModes = ['major', 'minor harmonic', 'minor melodic'];
+const diatonicScaleIntervals = ['octave apart', 'third apart', 'sixth apart'];
+const bothStyles = [STYLES.LEGATO, STYLES.STACCATO];
 
-const legatoScalesInThirds = ['C major', 'B♭ major'].map(key => ({key, intervals: ['thirds (hands separately)'], style: ['legato, 2 octaves']}));
-legatoScalesInThirds.forEach(s => allScales.push(s));
+const diatonicScales = standardScaleStartingNotes.map(startingNote =>
+    diatonicScaleModes.map(mode =>
+        bothStyles.map(style =>
+            diatonicScaleIntervals.map(interval =>
+                new DiatonicScaleOctaves(
+                    interval, startingNote, style, mode, interval === 'octave apart' ? DIATONIC_OCTAVES_SCALE_TEMPO : DIATONIC_SIXTH_THIRDS_SCALE_TEMPO)
+            )
+        )
+    )
+)
 
-const chromaticScales = [
-    {key: 'Chromatic', intervals: ['a minor third apart beginning on any notes'], style: bothStyles},
-    {key: 'Chromatic', intervals: ['beginning on A♯ and C♯ a minor third apart'], style: ['legato, hands separately, 2 octaves']}
-];
-chromaticScales.forEach(s => allScales.push(s));
+const legatoScalesInThirds = ['C', 'B♭'].map(startingNote => new DiatonicScaleThirds(startingNote))
 
-const wholeToneScale = {key: 'Whole tone', intervals: ['beginning on E'], style: ['legato, hands together']};
-allScales.push(wholeToneScale);
+const chromaticScaleMinorThirdApart = bothStyles.map(style =>
+    ALL_NOTES.map(note =>
+        new FingerExercise('third apart', 'hands together', 'chromatic', CHROMATIC_THIRD_APART_OCTAVES, note, style, CHROMATIC_THIRD_APART_TEMPO, 'chromatic scale a minor third apart')
+    )
+)
 
-const arpeggiModes = ['major', 'minor'];
-const arpeggiKeys = standardScaleStartingNotes.map(note => arpeggiModes.map(mode => `${note} ${mode}`)).flat();
-const arpeggi = arpeggiKeys.map(key => ({key, intervals: ['arpeggio, any inversion'], style: ['legato']}));
-arpeggi.map(a => allScales.push(a));
+const chromaticScaleInMinorThirds = ['left hand', 'right hand'].map(hand =>
+        new FingerExercise('minor thirds', hand, 'chromatic', CHROMATIC_MINOR_THIRDS_OCTAVES, 'A♯/C♯', STYLES.LEGATO, CHROMATIC_MINOR_THIRDS_TEMPO, 'chromatic scale in minor thirds')
+    )
 
-const dominantSevenths = standardScaleStartingNotes.map(n => `${n} major (KEY)`).map(key => ({key, intervals: ['Dominant seventh'], style: ['legato']}));
-dominantSevenths.map(a => allScales.push(a));
+const wholeToneScale = [new FingerExercise('octave apart', 'both hands', 'whole tone', WHOLE_TONE_SCALE_OCTAVES, 'E', STYLES.LEGATO, WHOLE_TONE_SCALE_TEMPO, 'whole-tone scale')]
 
-const diminishedSevenths = ['A', 'B♭', 'B'].map(n => ({key: `beginning on ${n}`, intervals: ['Diminished seventh'], style: ['legato']}));
-diminishedSevenths.map(a => allScales.push(a));
+const inversions = ['root', 'first inversion', 'second inversion']
+const arpeggioModes = ['major', 'minor']
+const arpeggi = standardScaleStartingNotes.map(startingNote =>
+    arpeggioModes.map(mode =>
+        inversions.map(inversion =>
+            new Arpeggio(inversion, mode, startingNote, 'standard')
+        )
+    )
+)
+
+const dominantSevenths = standardScaleStartingNotes.map(key => new DominantSeventh(key))
+
+const diminishedSevenths = ALL_NOTES.map(note => new Arpeggio('root', 'diminished seventh', note, 'diminished seventh'))
+
+const allScales = [diatonicScales, legatoScalesInThirds, chromaticScaleMinorThirdApart, chromaticScaleInMinorThirds, wholeToneScale, arpeggi, dominantSevenths, diminishedSevenths]
 
 const randomElement = array => array[Math.floor(Math.random() * array.length)];
 
-const randomScale = () => {
-    const scaleObj = randomElement(allScales);
-    const interval = randomElement(scaleObj.intervals);
-    const style = randomElement(scaleObj.style);
-    return `${scaleObj.key}, ${interval}${style === 'legato' ? '' : `, ${style}`}`
-};
+const randomScale = (scaleTree = allScales) => {
+    if (scaleTree.interval !== undefined) return scaleTree
+    return randomScale(randomElement(scaleTree))
+}
 
-const showRandomScale = () => document.getElementById("scalesOutput").innerHTML = JSON.stringify(randomScale());
+const showRandomScale = () => document.getElementById("scalesOutput").innerHTML = JSON.stringify(randomScale(), null, 4);
